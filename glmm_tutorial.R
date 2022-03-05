6ed3141bd80c264e64556d3847cd3f42baacc4d2
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-03-05 00:40:07 -0300

    Add files via upload

--- glmm_tutorial.R
+++ glmm_tutorial.R
@@ -30,3 +30,50 @@ View(dados)
 modelo <- glmer(Richness ~ NAP + (1|Beach), family = 'poisson', data = dados)
 summary(modelo)
 
+## Interpretação: em random effects podemos verificar os efeitos aleatórios ou o quanto da
+## variabilidade da variável resposta é devido aos efeitos aleatórios.
+## Abaixo encontramos os efeitos fixos do NAP com a estimativa, erro, valor z e valor de p.
+## Encontramos que ocorre a reduação de 0.5 espécies com uma unidade a mais do NAP.
+
+# Análises dos efeitos ---------------------------------------------------------------------------------------------------------------------
+
+(fixo <- fixef(modelo))  # Efeitos fixos
+(aleatorio = coef(modelo)$Beach)  # Efeitos aleatórios 
+
+## Os coeficientes apresentam no primeiro caso o intercepto e a inclinação sem os efeitos
+## aleatórios, e no segundo caso os interceptos e inclinações com os efeitos aleatórios em
+## cada praia.
+
+# Verificando amplitude dos valores de NAP -------------------------------------------------------------------------------------------------
+
+range(dados$NAP)
+seqNAP <- seq(-1.336, 2.255, len = 100) # Cria uma sequência de 100 valores de NAP dentro da amplitude.
+seqNAP
+
+# Calcular o modelo de cada uma das 9 praias -----------------------------------------------------------------------------------------------
+
+# Estimativas para cada praia
+
+praia01 <- aleatorio[1,1] + aleatorio[1,2] * seqNAP
+praia02 <- aleatorio[2,1] + aleatorio[2,2] * seqNAP
+praia03 <- aleatorio[3,1] + aleatorio[3,2] * seqNAP
+praia04 <- aleatorio[4,1] + aleatorio[4,2] * seqNAP
+praia05 <- aleatorio[5,1] + aleatorio[5,2] * seqNAP
+praia06 <- aleatorio[6,1] + aleatorio[6,2] * seqNAP
+praia07 <- aleatorio[7,1] + aleatorio[7,2] * seqNAP
+praia08 <- aleatorio[8,1] + aleatorio[8,2] * seqNAP
+praia09 <- aleatorio[9,1] + aleatorio[9,2] * seqNAP
+praiaFixo <- fixo[1] + fixo[2] * seqNAP
+
+# Estimativas por ponto
+
+praia01a <- exp(praia01)
+praia02a <- exp(praia02)
+praia03a <- exp(praia03)
+praia04a <- exp(praia04)
+praia05a <- exp(praia05) 
+praia06a <- exp(praia06)
+praia07a <- exp(praia07) 
+praia08a <- exp(praia08) 
+praia09a <- exp(praia09)
+praiaFixa <- exp(praiaFixo)
